e67665a7fa366b6ce5737dc2f511ee1255b65285
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-02 14:03:06 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(input);
+
+            string cmd;
+            while ((cmd = Console.ReadLine().ToLower()) != "end")
+            {
+                var arr = cmd.Split(' ');
+                var type = arr[0].ToLower();
+
+                if (type == "add")
+                {
+                    int one = int.Parse(arr[1]);
+                    int two = int.Parse(arr[2]);
+                    stack.Push(one);
+                    stack.Push(two);
+                }
+                else if (type == "remove")
+                {
+                    var countNums = int.Parse(arr[1]);
+                    if (stack.Count < countNums)
+                    {
+                        continue;
+                    }
+                    for (int i = 0; i < countNums; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            var sum = stack.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray()
+                .Reverse();
+
+            var stack = new Stack<string>(input);
+
+            int currStart = int.Parse(stack.Pop());
+            int sum = currStart;
+            while (stack.Count > 0)
+            {
+                string sym = stack.Pop();
+                int num = int.Parse(stack.Pop());
+                if (sym == "+")
+                {
+                    sum += num;
+                }
+                else if (sym == "-")
+                {
+                    sum -= num;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
