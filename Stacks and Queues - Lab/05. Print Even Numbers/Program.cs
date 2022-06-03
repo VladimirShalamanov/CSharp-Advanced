e9fb6e54668b3a4c65cc7877bea7eea71241399d
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-03 17:49:41 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                {
+                    queue.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/06. Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace _06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            string cmd;
+            while (true)
+            {
+                cmd = Console.ReadLine();
+                if (cmd == "Paid")
+                {
+                    foreach (var name in queue)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    queue.Clear();
+                    continue;
+                }
+                if (cmd == "End")
+                {
+                    Console.WriteLine($"{queue.Count} people remaining.");
+                }
+                queue.Enqueue(cmd);
+            }
+        }
+    }
+}
