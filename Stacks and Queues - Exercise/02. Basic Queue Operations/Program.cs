3e6689897a001d4ff671d75356813fbe08668683
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-07 22:43:22 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < arr[0]; i++)
+            {
+                queue.Enqueue(nums[i]);
+            }
+
+            for (int i = 0; i < arr[1]; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(arr[2]))
+            {
+                Console.WriteLine("true");
+            }
+
+            else
+            {
+                Console.WriteLine(queue.Count > 0 ? queue.Min() : 0);
+            }
+        }
+    }
+}
