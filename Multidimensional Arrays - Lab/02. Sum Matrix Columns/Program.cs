f8f9ffdab1abc56a241cb8b0966b8f01572141dc
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-11 18:08:36 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizes[0], sizes[1]];
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                var col = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = col[c];
+                }
+            }
+
+            for (int c = 0; c < matrix.GetLength(1); c++)
+            {
+                int sum = 0;
+                for (int x = 0; x < matrix.GetLength(0); x++)
+                {
+                    sum += matrix[x, c];
+                }
+                Console.WriteLine(sum);
+            }
+
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                int[] col = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    matrix[r, c] = col[c];
+                }
+            }
+            int sum = 0;
+            for (int r = 0; r < n; r++)
+            {
+                sum += matrix[r, r];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
