74915d80c547557f74458fae9c4168509cb6f1ec
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-12 17:52:07 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[height][];
+            int currWidth = 1;
+
+            for (int row = 0; row < height; row++)
+            {
+                triangle[row] = new long[currWidth];
+                long[] currRow = triangle[row];
+                currRow[0] = 1;
+                currRow[currRow.Length - 1] = 1;
+                currWidth++;
+                if (currRow.Length > 2)
+                {
+                    for (int i = 1; i < currRow.Length - 1; i++)
+                    {
+                        long[] previousRow = triangle[row - 1];
+                        long previousSum = previousRow[i] + previousRow[i - 1];
+                        currRow[i] = previousSum;
+                    }
+                }
+            }
+
+            foreach (long[] row in triangle)
+                Console.WriteLine(string.Join(" ", row));
+        }
+    }
+}
