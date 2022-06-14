c26393d51b9c4875176d9815172fb9b32e8d1e8d
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-14 15:20:52 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parking = new HashSet<string>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                string[] parts = cmd.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                if (parts[0] == "IN")
+                    parking.Add(parts[1]);
+                else
+                    parking.Remove(parts[1]);
+            }
+            if (parking.Count > 0)
+            {
+                foreach (var car in parking)
+                    Console.WriteLine(car);
+                return;
+            }
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var guests = new HashSet<string>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "PARTY")
+                guests.Add(cmd);
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                if (guests.Contains(cmd))
+                    guests.Remove(cmd);
+            }
+            Console.WriteLine(guests.Count);
+            foreach (var guest in guests)
+            {
+                char[] ch = guest.ToCharArray();
+                if (char.IsDigit(ch[0]))
+                    Console.WriteLine(guest);
+            }
+            foreach (var guest in guests)
+            {
+                char[] ch = guest.ToCharArray();
+                if (char.IsLetter(ch[0]))
+                    Console.WriteLine(guest);
+            }
+        }
+    }
+}
