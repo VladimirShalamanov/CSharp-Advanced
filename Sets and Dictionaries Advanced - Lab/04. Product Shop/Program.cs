5759cb4efa010d2664e3ba51e6e0330a2a9f0d99
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-14 15:13:26 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, Dictionary<string, double>>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Revision")
+            {
+                string[] arr = cmd.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string shop = arr[0];
+                string product = arr[1];
+                double price = double.Parse(arr[2]);
+
+                if (!dict.ContainsKey(shop))
+                    dict[shop] = new Dictionary<string, double>();
+                if (!dict[shop].ContainsKey(product))
+                    dict[shop].Add(product, 0);
+                dict[shop][product] = price;
+            }
+
+            foreach (var shop in dict.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var continentData = new Dictionary<string, Dictionary<string, List<string>>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string continent = arr[0];
+                string country = arr[1];
+                string city = arr[2];
+                if (!continentData.ContainsKey(continent))
+                    continentData[continent] = new Dictionary<string, List<string>>();
+                if (!continentData[continent].ContainsKey(country))
+                    continentData[continent][country] = new List<string>();
+                continentData[continent][country].Add(city);
+            }
+
+            foreach (var (continetName, countries) in continentData)
+            {
+                Console.WriteLine($"{continetName}:");
+                foreach (var (countryName, cities) in countries)
+                {
+                    Console.Write($"  {countryName} -> ");
+                    Console.Write($"{string.Join(", ", cities)}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
