0aef6dd35112f9e60c5ae86d27c5ba325e1b98a1
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-17 13:53:58 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._SoftUni_Exam_Results
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contest = new Dictionary<string, Dictionary<string, int>>();
+            var counterLanguage = new Dictionary<string, int>();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "exam finished")
+            {
+                string[] arr = cmd.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                string name = arr[0];
+
+                if (arr.Length == 2 && arr[1] == "banned")
+                {
+                    if (contest.ContainsKey(name))
+                        contest.Remove(name);
+                }
+                else if (arr.Length == 3)
+                {
+                    string language = arr[1];
+                    int poitns = int.Parse(arr[2]);
+
+                    if (!contest.ContainsKey(name))
+                    {
+                        contest[name] = new Dictionary<string, int>();
+                        contest[name].Add(language, poitns);
+
+                        if (!counterLanguage.ContainsKey(language))
+                            counterLanguage.Add(language, 1);
+                        else if (counterLanguage.ContainsKey(language))
+                            counterLanguage[language]++;
+                    }
+                    else if (!contest[name].ContainsKey(language))
+                    {
+                        contest[name] = new Dictionary<string, int>();
+                        contest[name].Add(language, poitns);
+
+                        if (!counterLanguage.ContainsKey(language))
+                            counterLanguage.Add(language, 1);
+                        else if (counterLanguage.ContainsKey(language))
+                            counterLanguage[language]++;
+                    }
+                    else if (contest[name].ContainsKey(language))
+                    {
+                        counterLanguage[language]++;
+                        if (contest[name][language] < poitns)
+                            contest[name][language] = poitns;
+                    }
+                }
+            }
+
+            Console.WriteLine("Results:");
+            var total = new Dictionary<string, int>();
+            foreach (var rtd in contest)
+            {
+                total[rtd.Key] = 0;
+                foreach (var item in rtd.Value)
+                {
+                    total[rtd.Key] = item.Value;
+                }
+            }
+            foreach (var racer in total.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{racer.Key} | {racer.Value}");
+
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var launguage in counterLanguage.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{launguage.Key} - {launguage.Value}");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercise/10. ForceBook/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._ForceBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var force = new Dictionary<string, List<string>>();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Lumpawaroo")
+            {
+                bool splitByLine = false;
+                bool splitByArrow = false;
+                var cmdSplitByLine = cmd.Split(" | ");
+                var cmdSplitByArrow = cmd.Split(" -> ");
+                if (cmdSplitByLine.Length == 2)
+                    splitByLine = true;
+                else if (cmdSplitByArrow.Length == 2)
+                    splitByArrow = true;
+
+                if (splitByLine)
+                {
+                    bool containsUser = false;
+                    foreach (var team in force)
+                    {
+                        if (team.Value.Contains(cmdSplitByLine[1]))
+                            containsUser = true;
+                    }
+                    if (force.ContainsKey(cmdSplitByLine[0]))
+                    {
+                        if (containsUser == false)
+                            force[cmdSplitByLine[0]].Add(cmdSplitByLine[1]);
+                    }
+                    else
+                    {
+                        force.Add(cmdSplitByLine[0], new List<string>());
+                        if (containsUser == false)
+                            force[cmdSplitByLine[0]].Add(cmdSplitByLine[1]);
+                    }
+                }
+                else if (splitByArrow)
+                {
+                    foreach (var team in force)
+                    {
+                        if (team.Value.Contains(cmdSplitByArrow[0]))
+                            team.Value.Remove(cmdSplitByArrow[0]);
+                    }
+                    if (!force.ContainsKey(cmdSplitByArrow[1]))
+                        force.Add(cmdSplitByArrow[1], new List<string>());
+
+                    force[cmdSplitByArrow[1]].Add(cmdSplitByArrow[0]);
+                    Console.WriteLine($"{cmdSplitByArrow[0]} joins the {cmdSplitByArrow[1]} side!");
+                }
+            }
+
+            foreach (var kvp in force
+                .Where(x => x.Value.Count >= 1)
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"Side: {kvp.Key}, Members: {kvp.Value.Count}");
+                foreach (var item in kvp.Value.OrderBy(x => x))
+                    Console.WriteLine($"! {item}");
+            }
+        }
+    }
+}
