406f55c4004518ad9ac792282727efad78b2b75a
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-19 15:47:40 +0300

    Add files via upload

---
+++ Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, int> parser = n => int.Parse(n);
+            int[] num = input.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser).ToArray();
+            Console.WriteLine(num.Length);
+            Console.WriteLine(num.Sum());
+        }
+    }
+}
