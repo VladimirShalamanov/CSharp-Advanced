a0b3d50bebb148921ee4eab76fc4b2790cb8e2a0
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-21 14:33:52 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/10. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._The_Party_Reservation_Filter_Module_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var filters = new List<string>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Print")
+            {
+                string[] arr = cmd.Split(';', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string type = arr[0];
+                if (type == "Add filter")
+                {
+                    filters.Add(arr[1] + " " + arr[2]);
+                }
+                else if (type == "Remove filter")
+                {
+                    filters.Remove(arr[1] + " " + arr[2]);
+                }
+            }
+
+            foreach (var filter in filters)
+            {
+                var commands = filter.Split(' ');
+
+                if (commands[0] == "Starts")
+                {
+                    guests = guests.Where(p => !p.StartsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Ends")
+                {
+                    guests = guests.Where(p => !p.EndsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Length")
+                {
+                    guests = guests.Where(p => p.Length != int.Parse(commands[1])).ToList();
+                }
+                else if (commands[0] == "Contains")
+                {
+                    guests = guests.Where(p => !p.Contains(commands[1])).ToList();
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', guests));
+        }
+    }
+}
---
+++ Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray()
+                .Select(ch => (int)ch).Sum() >= num;
+
+            Func<string[], int, Func<string, int, bool>, string> firstValidName = (arr, num, func) => arr
+                .FirstOrDefault(str => func(str, num));
+
+            Console.WriteLine(firstValidName(names, num, isValidWord));
+        }
+    }
+}
