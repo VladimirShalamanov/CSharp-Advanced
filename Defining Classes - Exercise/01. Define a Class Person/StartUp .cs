d9646e8daccf092eaae1bc3a1cf4a01b33d046ec
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-22 16:04:05 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/01. Define a Class Person/StartUp .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Peter", 20);
+            Person person1 = new Person();
+
+            person1.Name = "George";
+            person1.Age = 18;
+        }
+    }
+}
---
+++ Defining Classes - Exercise/02. Creating Constructors/StartUp .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person(24);
+            Person person3 = new Person("George", 24);
+        }
+    }
+}
