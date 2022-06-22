43d9a665c62e5ad3e93ad8cd0258f714023a5399
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-22 16:09:30 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/06. Speed Racing/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string data = Console.ReadLine();
+                string model = data.Split()[0];
+                double fuelAmount = double.Parse(data.Split()[1]);
+                double fuelConsumption = double.Parse(data.Split()[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumption);
+                cars.Add(car);
+            }
+
+            string cmd;
+            while((cmd = Console.ReadLine()) != "End")
+            {
+                string type = cmd.Split()[0];
+                string carModel = cmd.Split()[1];
+                double amountOfKm = double.Parse(cmd.Split()[2]);
+
+                Car carToDrive = cars.First(car => car.Model == carModel);
+                carToDrive.Drive(amountOfKm);
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
