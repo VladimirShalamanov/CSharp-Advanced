a74837adcbee0ce6b9400bb32a3ad78dbb50e468
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-24 11:21:13 +0300

    Add files via upload

---
+++ Generics - Lab/03. Generic Scale/StartUp .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
