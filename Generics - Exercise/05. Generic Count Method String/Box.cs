260bb56bef68add8971ae630144ef69e6fdd9362
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-27 22:09:08 +0300

    Add files via upload

---
+++ Generics - Exercise/05. Generic Count Method String/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T> : IComparable<T> where T : IComparable<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public Box(List<T> elementsList)
+        {
+            Elements = elementsList;
+        }
+
+        public T Element { get; }
+
+        public List<T> Elements { get; }
+
+        public void Swap(List<T> elements, int indexOne, int indexTwo)
+        {
+            T firstEl = elements[indexOne];
+            elements[indexOne] = elements[indexTwo];
+            elements[indexTwo] = firstEl;
+        }
+
+        public int CountOfGreater<T>(List<T> list, T readLine) where T : IComparable
+            => list.Count(w => w.CompareTo(readLine) > 0);
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (T element in Elements)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+            return sb.ToString().TrimEnd();
+            //return $"{typeof(T)}: {Element}";
+        }
+
+        public int CompareTo(T other) => Element.CompareTo(other);
+
+    }
+}
