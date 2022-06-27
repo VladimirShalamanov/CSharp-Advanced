a75a8478b668d753a40fb6ef76090e5cdd4dba43
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-27 21:53:08 +0300

    Add files via upload

---
+++ Generics - Exercise/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                list.Add(name);
+            }
+            var box = new Box<string>(list);
+            var indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
