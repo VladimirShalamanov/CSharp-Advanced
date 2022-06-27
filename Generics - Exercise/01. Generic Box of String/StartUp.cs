d70bcd31edcf5a72c4c6486623372a3bbf035f1d
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-27 21:46:37 +0300

    Add files via upload

---
+++ Generics - Exercise/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                var box = new Box<string>(name);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
