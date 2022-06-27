3538aeaba681a8a338ddfae0e29a87b7b8626194
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-27 21:51:45 +0300

    Add files via upload

---
+++ Generics - Exercise/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int name = int.Parse(Console.ReadLine());
+                var box = new Box<int>(name);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
