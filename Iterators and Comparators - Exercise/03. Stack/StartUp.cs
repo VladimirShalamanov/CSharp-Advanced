b87134e64f49fae683767cc60a62ed2d2e4289eb
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-30 16:53:34 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                var tokens = cmd
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (tokens[0] == "Push")
+                    stack.Push(tokens.Skip(1)
+                        .Select(e => e.Split(",", StringSplitOptions.RemoveEmptyEntries).First())
+                        .ToArray());
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+            }
+
+            foreach (var el in stack)
+            {
+                Console.WriteLine(el);
+            }
+            foreach (var el in stack)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
