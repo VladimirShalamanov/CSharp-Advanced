f7d8f07e89e9658ee88b6d798251a71955822847
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-06-30 16:59:13 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                var arr = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string personName = arr[0];
+                int personAge = int.Parse(arr[1]);
+                string personTown = arr[2];
+                persons.Add(new Person(personName, personAge, personTown));
+            }
+            int index = int.Parse(Console.ReadLine()) - 1;
+            int equal = 0;
+            int notEqual = 0;
+            foreach (var person in persons)
+            {
+                if (persons[index].CompareTo(person) == 0)
+                {
+                    equal++;
+                }
+                else
+                {
+                    notEqual++;
+                }
+            }
+            if (equal <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equal} {notEqual} {persons.Count}");
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/06. Equality Logic/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var sortedSet = new HashSet<Person>();
+            var hashSet = new HashSet<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var person = new Person(tokens[0], int.Parse(tokens[1]));
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
