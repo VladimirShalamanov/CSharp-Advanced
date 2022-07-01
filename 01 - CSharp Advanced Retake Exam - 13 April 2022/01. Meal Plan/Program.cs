2299512e7e57f1200c6d9e083743deab217d9d81
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-07-01 14:56:36 +0300

    Add files via upload

---
+++ 01 - CSharp Advanced Retake Exam - 13 April 2022/01. Meal Plan/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Meal_Plan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<int> maxCal = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            Stack<int> calories = new Stack<int>(maxCal);
+            Queue<string> meals = new Queue<string>(input);
+
+
+            string currMeal = string.Empty;
+            int currMealCal = 0;
+            if (meals.Any())
+            {
+                currMeal = meals.Dequeue();
+                currMealCal = GetCurrMealCal(currMeal);
+            }
+            else
+            {
+                return;
+            }
+
+            int currDayCal = 0;
+            while (calories.Any())
+            {
+                currDayCal = calories.Pop();
+                if (currDayCal == 0)
+                {
+                    break;
+                }
+                if (currDayCal > currMealCal)
+                {
+                    currDayCal -= currMealCal;
+                    calories.Push(currDayCal);
+                    if (meals.Any())
+                    {
+                        currMeal = meals.Dequeue();
+                        currMealCal = GetCurrMealCal(currMeal);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (currDayCal == currMealCal)
+                {
+                    if (meals.Any())
+                    {
+                        currMeal = meals.Dequeue();
+                        currMealCal = GetCurrMealCal(currMeal);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    currMealCal -= currDayCal;
+                }
+
+            }
+            if (meals.Count == 0)
+            {
+                Console.WriteLine($"John had {input.Count} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calories)} calories.");
+            }
+            else
+            {
+                Console.WriteLine($"John ate enough, he had {input.Count - meals.Count} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+
+        }
+        static int GetCurrMealCal(string meal)
+        {
+            int saladCal = 350;
+            int soupCal = 490;
+            int pastaCal = 680;
+            int steakCal = 790;
+
+            int currMealCal = 0;
+            if (meal == "salad")
+            {
+                currMealCal = saladCal;
+            }
+            else if (meal == "soup")
+            {
+                currMealCal = soupCal;
+            }
+            else if (meal == "pasta")
+            {
+                currMealCal = pastaCal;
+            }
+            else if (meal == "steak")
+            {
+                currMealCal = steakCal;
+            }
+            return currMealCal;
+        }
+    }
+}
---
+++ 01 - CSharp Advanced Retake Exam - 13 April 2022/02. Truffle Hunter/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Truffle_Hunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[,] forest = new string[n, n];
+
+            for (int row = 0; row < forest.GetLength(0); row++)
+            {
+                string[] ch = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                for (int col = 0; col < forest.GetLength(1); col++)
+                    forest[row, col] = ch[col];
+            }
+
+
+            int blackTruffle = 0;
+            int summerTruffle = 0;
+            int whiteTruffle = 0;
+            int wildBoar = 0;
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Stop the hunt")
+            {
+                string[] arr = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (arr.Length == 3)
+                {
+                    if (arr[0] == "Collect")
+                    {
+                        if (int.TryParse(arr[1], out int rowInput))
+                        {
+                            if (int.TryParse(arr[2], out int colInput))
+                            {
+                                if (rowInput >= 0 && rowInput < forest.GetLength(0) &&
+                                    colInput >= 0 && colInput < forest.GetLength(1))
+                                {
+                                    if (forest[rowInput, colInput] == "B")
+                                    {
+                                        blackTruffle++;
+                                        forest[rowInput, colInput] = "-";
+                                    }
+                                    else if (forest[rowInput, colInput] == "S")
+                                    {
+                                        summerTruffle++;
+                                        forest[rowInput, colInput] = "-";
+                                    }
+                                    else if (forest[rowInput, colInput] == "W")
+                                    {
+                                        whiteTruffle++;
+                                        forest[rowInput, colInput] = "-";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (arr.Length == 4)
+                {
+                    if (arr[0] == "Wild_Boar")
+                    {
+                        if (int.TryParse(arr[1], out int rowInput))
+                        {
+                            if (int.TryParse(arr[2], out int colInput))
+                            {
+                                if (rowInput >= 0 && rowInput < forest.GetLength(0) &&
+                                    colInput >= 0 && colInput < forest.GetLength(1))
+                                {
+                                    if (arr[3] == "up")
+                                    {
+                                        for (int i = rowInput; i >= 0; i--)
+                                        {
+                                            if (forest[i, colInput] == "B" ||
+                                                forest[i, colInput] == "S" ||
+                                                forest[i, colInput] == "W")
+                                            {
+                                                wildBoar++;
+                                                forest[i, colInput] = "-";
+                                            }
+                                            i--;
+                                        }
+                                    }
+                                    else if (arr[3] == "down")
+                                    {
+                                        for (int i = rowInput; i < forest.GetLength(0); i++)
+                                        {
+                                            if (forest[i, colInput] == "B" ||
+                                                forest[i, colInput] == "S" ||
+                                                forest[i, colInput] == "W")
+                                            {
+                                                wildBoar++;
+                                                forest[i, colInput] = "-";
+                                            }
+                                            i++;
+                                        }
+                                    }
+                                    else if (arr[3] == "left")
+                                    {
+                                        for (int i = colInput; i >= 0; i--)
+                                        {
+                                            if (forest[rowInput, i] == "B" ||
+                                                forest[rowInput, i] == "S" ||
+                                                forest[rowInput, i] == "W")
+                                            {
+                                                wildBoar++;
+                                                forest[rowInput, i] = "-";
+                                            }
+                                            i--;
+                                        }
+                                    }
+                                    else if (arr[3] == "right")
+                                    {
+                                        for (int i = colInput; i < forest.GetLength(1); i++)
+                                        {
+                                            if (forest[rowInput, i] == "B" ||
+                                                forest[rowInput, i] == "S" ||
+                                                forest[rowInput, i] == "W")
+                                            {
+                                                wildBoar++;
+                                                forest[rowInput, i] = "-";
+                                            }
+                                            i++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Peter manages to harvest {blackTruffle} black, {summerTruffle} summer, and {whiteTruffle} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {wildBoar} truffles.");
+
+            for (int row = 0; row < forest.GetLength(0); row++)
+            {
+                for (int col = 0; col < forest.GetLength(1); col++)
+                {
+                    Console.Write(forest[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
