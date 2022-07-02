6ed09f9ec70a93dee4b2720c94ce9b4f6f5abf67
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-07-02 15:17:01 +0300

    Add files via upload

---
+++ 02 - CSharp Advanced Exam - 20 February 2022/FishingNet/FishingNet/Net.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        public List<Fish> fish { get; set; }
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+
+        public Net(string material, int capacity)
+        {
+            this.Material = material;
+            this.Capacity = capacity;
+            this.fish = new List<Fish>();
+        }
+
+        public int Count => this.fish.Count;
+        public IReadOnlyCollection<Fish> Fish => (IReadOnlyCollection<Fish>)this.fish;
+
+        public string AddFish(Fish fish)
+        {
+            if (fish.FishType == null || string.IsNullOrEmpty(fish.FishType) || fish.Length <= 0 || fish.Weight <= 0)
+            {
+                return "Invalid fish.";
+            }
+            if (Fish.Count + 1 > Capacity) return "Fishing net is full.";
+            this.fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+            var fish = this.fish.FirstOrDefault(e => e.Weight == weight);
+            if (fish != null)
+            {
+                return this.fish.Remove(fish);
+            }
+            return false;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            Fish fish = Fish.Where(f => f.FishType == fishType).First();
+            return fish;
+        }
+
+        public Fish GetBiggestFish()
+        {
+            var longestFish = this.fish.Max(e => e.Length);
+            var fish = this.fish.FirstOrDefault(e => e.Length == longestFish);
+            return fish;
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Into the {this.Material}:");
+            foreach (var f in Fish.OrderByDescending(f => f.Length))
+            {
+                sb.AppendLine(f.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
