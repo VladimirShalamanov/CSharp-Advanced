6a50edac7b2a0168ae4b0da5caee0c11ad5e3d88
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-07-11 15:33:50 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Exercise/07. Knight Game/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace _07._Knight_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string arr = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                    matrix[row, col] = arr[col].ToString();
+            }
+
+            int knightsInDangerCounter = 0;
+            int removedKnightsCounter = 0;
+
+            for (int maxAttackPotential = 8; maxAttackPotential > 0; maxAttackPotential--) // Every knight can attack and kill maximum 8 other knights. P.S. Check "Possible moves" at the beginning.
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (matrix[row, col].ToLower() == "k") // Check if we have knight on current position.
+                        {
+                            // Check if new position isn't outside of the board (so we don't get IndexOutOfRangeException).
+                            if (row - 1 >= 0)
+                            {
+                                if (col - 2 >= 0)
+                                {
+                                    // Check if there is knight to be attacked on the new possition.
+                                    if (matrix[row - 1, col - 2].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+
+                                if (col + 2 < matrix.GetLength(1))
+                                {
+                                    // Check if there is knight to be attacked on the new possition.
+                                    if (matrix[row - 1, col + 2].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+                            }
+
+                            if (row + 1 < matrix.GetLength(0))
+                            {
+                                if (col - 2 >= 0)
+                                {
+                                    // Check if there is knight to be attacked on the new possition
+                                    if (matrix[row + 1, col - 2].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+
+                                if (col + 2 < matrix.GetLength(1))
+                                {
+                                    // Check if there is knight to be attacked on the new possition
+                                    if (matrix[row + 1, col + 2].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+                            }
+
+                            if (row - 2 >= 0)
+                            {
+                                if (col - 1 >= 0)
+                                {
+                                    // Check if there is knight to be attacked on the new possition
+                                    if (matrix[row - 2, col - 1].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+
+                                if (col + 1 < matrix.GetLength(1))
+                                {
+                                    // Check if there is knight to be attacked on the new possition
+                                    if (matrix[row - 2, col + 1].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+                            }
+
+                            if (row + 2 < matrix.GetLength(0))
+                            {
+                                if (col - 1 >= 0)
+                                {
+                                    // Check if there is knight to be attacked on the new possition
+                                    if (matrix[row + 2, col - 1].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+
+                                if (col + 1 < matrix.GetLength(1))
+                                {
+                                    // Check if there is knight to be attacked on the new possition
+                                    if (matrix[row + 2, col + 1].ToLower() == "k")
+                                    {
+                                        knightsInDangerCounter++;
+                                    }
+                                }
+                            }
+                        }
+
+                        // Check if every possible attack is against another knight (attack potential of current knight is biggest possible)
+                        if (knightsInDangerCounter == maxAttackPotential)
+                        {
+                            matrix[row, col] = "0"; // Remove knight from board.
+                            removedKnightsCounter++;
+                        }
+
+                        knightsInDangerCounter = 0; // Restart counter.
+                    }
+                }
+            }
+
+            // Print result
+            Console.WriteLine(removedKnightsCounter);
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/08. Bombs/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] battlefield = new int[n, n];
+
+            for (int row = 0; row < battlefield.GetLength(0); row++)
+            {
+                int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < battlefield.GetLength(1); col++)
+                    battlefield[row, col] = arr[col];
+            }
+
+            string[] info = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            for (int i = 0; i < info.Length; i++)
+            {
+                int[] coordinates = info[i].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int row = 0; row < battlefield.GetLength(0); row++)
+                {
+                    for (int col = 0; col < battlefield.GetLength(1); col++)
+                    {
+                        if (row == coordinates[0] && col == coordinates[1])
+                        {
+                            if (battlefield[row, col] > 0)
+                            {
+                                int bomb = battlefield[row, col];
+                                battlefield[row, col] = 0;
+                                if (row - 1 >= 0)
+                                {
+                                    if (battlefield[row - 1, col] > 0)
+                                        battlefield[row - 1, col] -= bomb;
+                                }
+                                if (row + 1 < battlefield.GetLength(0))
+                                {
+                                    if (battlefield[row + 1, col] > 0)
+                                        battlefield[row + 1, col] -= bomb;
+                                }
+                                if (col - 1 >= 0)
+                                {
+                                    if (battlefield[row, col - 1] > 0)
+                                        battlefield[row, col - 1] -= bomb;
+                                }
+                                if (col + 1 < battlefield.GetLength(1))
+                                {
+                                    if (battlefield[row, col + 1] > 0)
+                                        battlefield[row, col + 1] -= bomb;
+                                }
+                                if (row - 1 >= 0 && col - 1 >= 0)
+                                {
+                                    if (battlefield[row - 1, col - 1] > 0)
+                                        battlefield[row - 1, col - 1] -= bomb;
+                                }
+                                if (row + 1 < battlefield.GetLength(0) && col + 1 < battlefield.GetLength(1))
+                                {
+                                    if (battlefield[row + 1, col + 1] > 0)
+                                        battlefield[row + 1, col + 1] -= bomb;
+                                }
+                                if (col - 1 >= 0 && row + 1 < battlefield.GetLength(0))
+                                {
+                                    if (battlefield[row + 1, col - 1] > 0)
+                                        battlefield[row + 1, col - 1] -= bomb;
+                                }
+                                if (col + 1 < battlefield.GetLength(1) && row - 1 >= 0)
+                                {
+                                    if (battlefield[row - 1, col + 1] > 0)
+                                        battlefield[row - 1, col + 1] -= bomb;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int count = 0;
+            int sum = 0;
+            for (int row = 0; row < battlefield.GetLength(0); row++)
+            {
+                for (int col = 0; col < battlefield.GetLength(1); col++)
+                {
+                    if (battlefield[row, col] > 0)
+                    {
+                        count++;
+                        sum += battlefield[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine("Alive cells: " + count);
+            Console.WriteLine("Sum: " + sum);
+
+            for (int row = 0; row < battlefield.GetLength(0); row++)
+            {
+                for (int col = 0; col < battlefield.GetLength(1); col++)
+                    Console.Write(battlefield[row, col] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
