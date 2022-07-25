11b38e70b88182e8097f4dff58ffa30a0faf038e
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-07-25 14:34:40 +0300

    Add files via upload

---
+++ 07 - CSharp Advanced Exam - 22 Feb 2020/02. Re-Volt/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _02._Re_Volt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countCmd = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            int r = 0;
+            int c = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string arr = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = arr[col];
+                    if (matrix[row, col] == 'f') { r = row; c = col; }
+                }
+            }
+
+            while (countCmd != 0)
+            {
+                string move = Console.ReadLine();
+                int row = r;
+                int col = c;
+                matrix[row, col] = '-';
+                if (move == "up") row--;
+                else if (move == "down") row++;
+                else if (move == "left") col--;
+                else if (move == "right") col++;
+                //----------------------------------------------
+                if (row < 0) row = matrix.GetLength(0) - 1;
+                else if (row > matrix.GetLength(0) - 1) row = 0;
+                else if (col < 0) col = matrix.GetLength(1) - 1;
+                else if (col > matrix.GetLength(1) - 1) col = 0;
+                //----------------------------------------------
+                if (matrix[row, col] == 'B')
+                {
+                    if (move == "up") row--;
+                    else if (move == "down") row++;
+                    else if (move == "left") col--;
+                    else if (move == "right") col++;
+                    if (row < 0) row = matrix.GetLength(0) - 1;
+                    else if (row > matrix.GetLength(0) - 1) row = 0;
+                    else if (col < 0) col = matrix.GetLength(1) - 1;
+                    else if (col > matrix.GetLength(1) - 1) col = 0;
+                }
+                else if (matrix[row, col] == 'T')
+                {
+                    row = r;
+                    col = c;
+                }
+                if (matrix[row, col] == 'F')
+                {
+                    r = row;
+                    c = col;
+                    break;
+                }
+                if (matrix[row, col] == '-') matrix[row, col] = 'f';
+                r = row;
+                c = col;
+                countCmd--;
+            }
+
+            if (matrix[r, c] == 'F')
+            {
+                matrix[r, c] = 'f';
+                Console.WriteLine("Player won!");
+            }
+            else Console.WriteLine("Player lost!");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                    Console.Write(matrix[row, col]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 07 - CSharp Advanced Exam - 22 Feb 2020/Guild/Guild.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        public List<Player> roster { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public Guild(string name, int capacity)
+        {
+            roster = new List<Player>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count + 1 <= this.Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+
+            var removes = roster.FirstOrDefault(p => p.Name == name);
+            if (removes == null) return false;
+            roster.Remove(removes);
+            return true;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            var player = roster.FirstOrDefault(p => p.Name == name);
+            if (player != null)
+            {
+                if (player.Rank != "Member")
+                {
+                    player.Rank = "Member";
+                }
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            var player = roster.FirstOrDefault(p => p.Name == name);
+            if (player != null)
+            {
+                if (player.Rank != "Trial")
+                {
+                    player.Rank = "Trial";
+                }
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            var players = roster.FindAll(p => p.Class == @class);
+            roster.RemoveAll(p => p.Class == @class);
+            Player[] player = players.ToArray();
+            return player;
+        }
+
+        public int Count
+        {
+            get => this.roster.Count();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {this.Name}");
+            foreach (var p in roster) sb.AppendLine(p.ToString());
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
