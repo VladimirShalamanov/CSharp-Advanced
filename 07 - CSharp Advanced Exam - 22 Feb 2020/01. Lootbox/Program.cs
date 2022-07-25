ba1e05de68ebd392e0019347c063447c54636853
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-07-25 14:33:11 +0300

    Add files via upload

---
+++ 07 - CSharp Advanced Exam - 22 Feb 2020/01. Lootbox/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Lootbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstNumsList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> twoNumsList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var queueNums = new Queue<int>(firstNumsList);
+            var stackNums = new Stack<int>(twoNumsList);
+
+            double sum = 0;
+
+            while (queueNums.Any() && stackNums.Any())
+            {
+                int firstItem = queueNums.Peek();
+                int secondItem = stackNums.Pop();
+                double s = firstItem + secondItem;
+                if (s % 2 == 0)
+                {
+                    sum += s;
+                    queueNums.Dequeue();
+                }
+                else
+                {
+                    queueNums.Enqueue(secondItem);
+                }
+            }
+
+            if (queueNums.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (stackNums.Count == 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (sum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {sum}");
+            }
+        }
+    }
+}
