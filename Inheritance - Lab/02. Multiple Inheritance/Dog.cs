dfb1ab2a91a7af590f3cbfa621f5d72ab010dff3
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-03 17:04:07 +0300

    Add files via upload

---
+++ Inheritance - Lab/02. Multiple Inheritance/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public string dog { get; set; }
+        public void Bark()
+        {
+            Console.WriteLine("barking…");
+        }
+    }
+}
