328fa39f04ca5e198d9e0fa54c300902b52129dd
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-08 15:36:48 +0300

    Add files via upload

---
+++ Inheritance - Exercise/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double GramsC = 250;
+        private const double CaloriesC = 1000;
+        private const decimal CakePrice = 5m;
+
+        public Cake(string name) : base(name, CakePrice, GramsC, CaloriesC)
+        {
+
+        }
+    }
+}
