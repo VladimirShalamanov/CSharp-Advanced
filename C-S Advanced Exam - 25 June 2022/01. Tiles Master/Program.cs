042d59244303a94a872844a51dcaaf2d6ac01e15
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-09 14:45:49 +0300

    Add files via upload

---
+++ C-S Advanced Exam - 25 June 2022/01. Tiles Master/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Tiles_Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listWhite = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> listGrey = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var whiteQueue = new Stack<int>(listWhite);
+            var greyQueue = new Queue<int>(listGrey);
+
+            var location = new Dictionary<string, int>();
+
+            while (whiteQueue.Any() && greyQueue.Any())
+            {
+                int white = whiteQueue.Pop();
+                int grey = greyQueue.Dequeue();
+
+                string s = string.Empty;
+
+                if (white == grey)
+                {
+                    int sum = white + grey;
+                    if (sum == 40)
+                    {
+                        s = "Sink";
+                    }
+                    else if (sum == 50)
+                    {
+                        s = "Oven";
+                    }
+                    else if (sum == 60)
+                    {
+                        s = "Countertop";
+                    }
+                    else if (sum == 70)
+                    {
+                        s = "Wall";
+                    }
+                    else
+                    {
+                        s = "Floor";
+                    }
+                    if (!location.ContainsKey(s))
+                    {
+                        location.Add(s, 0);
+                    }
+                    location[s]++;
+                }
+                else
+                {
+                    white /= 2;
+                    whiteQueue.Push(white);
+                    greyQueue.Enqueue(grey);
+                }
+            }
+
+            if (!whiteQueue.Any()) Console.WriteLine("White tiles left: none");
+            else Console.WriteLine($"White tiles left: {string.Join(", ", whiteQueue)}");
+            if (!greyQueue.Any()) Console.WriteLine("Grey tiles left: none");
+            else Console.WriteLine($"Grey tiles left: {string.Join(", ", greyQueue)}");
+
+            foreach (var p in location.OrderByDescending(x => x.Value).ThenBy(s => s.Key))
+            {
+                Console.WriteLine($"{p.Key}: {p.Value}");
+            }
+        }
+    }
+}
---
+++ C-S Advanced Exam - 25 June 2022/Renovators/Catalog.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        public List<Renovator> renovators { get; set; }
+        public IReadOnlyCollection<Renovator> Renovators => renovators;
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            this.Name = name;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+            this.renovators = new List<Renovator>();
+        }
+
+        public int Count
+        {
+            get => this.renovators.Count;
+        }
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+            {
+                return "Invalid renovator's information.";
+            }
+            else if (this.renovators.Count == this.NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+            else if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+            this.renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            bool s = false;
+            var remove = this.renovators.FirstOrDefault(x => x.Name == name);
+            if (remove != null)
+            {
+                this.renovators.Remove(remove);
+                s = true;
+                return s;
+            }
+            return s;
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            List<Renovator> remove = this.renovators.FindAll(x => x.Type == type).ToList();
+            if (remove.Count > 0)
+            {
+                foreach (var r in remove)
+                {
+                    this.renovators.Remove(r);
+                }
+                return remove.Count;
+            }
+            return 0;
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            var hire = this.renovators.FirstOrDefault(x => x.Name == name);
+            if (hire != null)
+            {
+                foreach (var r in this.renovators)
+                {
+                    if (r == hire)
+                    {
+                        r.Hired = true;
+                    }
+                }
+                return hire;
+            }
+            return null;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> hire = this.renovators.FindAll(x => x.Days >= days);
+            return hire.ToList();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Renovators available for Project {this.Project}:");
+            foreach (var r in renovators.Where(r => r.Hired == false))
+            {
+                sb.AppendLine(r.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
