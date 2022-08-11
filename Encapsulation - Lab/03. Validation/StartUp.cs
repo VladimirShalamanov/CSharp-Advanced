43387cd1f0a2cc51d834ccaeb611d624cdebabce
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-11 13:05:02 +0300

    Add files via upload

---
+++ Encapsulation - Lab/03. Validation/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var peoples = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Person person = null;
+                try
+                {
+                    string[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string f = arr[0];
+                    string l = arr[1];
+                    int a = int.Parse(arr[2]);
+                    decimal s = decimal.Parse(arr[3]);
+                    person = new Person(f, l, a, s);
+                    peoples.Add(person);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            decimal parcentage = decimal.Parse(Console.ReadLine());
+            peoples.ForEach(p => p.IncreaseSalary(parcentage));
+            peoples.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
---
+++ Encapsulation - Lab/04. Team/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Team team = new Team("SoftUni");
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string f = arr[0];
+                string l = arr[1];
+                int a = int.Parse(arr[2]);
+                decimal s = decimal.Parse(arr[3]);
+                var person = new Person(f, l, a, s);
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team have {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team have {team.ReserveTeam.Count} players.");
+        }
+    }
+}
