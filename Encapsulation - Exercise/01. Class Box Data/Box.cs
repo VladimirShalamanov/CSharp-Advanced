9df606e9a518411e6076090f9c6e9cc246159f12
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-13 15:13:47 +0300

    Add files via upload

---
+++ Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        private double Length
+        {
+            get { return this.length; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+        private double Width
+        {
+            get { return this.width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+        private double Height
+        {
+            get { return this.height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            double s = (2 * this.Length * this.Width) + (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+            return s;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double l = (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+            return l;
+        }
+
+        public double Volume()
+        {
+            double v = this.Length * this.Width * this.Height;
+            return v;
+        }
+    }
+}
