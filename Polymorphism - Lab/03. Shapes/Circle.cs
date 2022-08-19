8bab494c36b4003690f5770dd3f17d15a11aa8ed
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-19 10:36:30 +0300

    Add files via upload

---
+++ Polymorphism - Lab/03. Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+        public double Radius
+        {
+            get => this.radius;
+            private set => this.radius = value;
+        }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public override double CalculateArea() => Math.PI * this.Radius * this.Radius;
+
+        public override double CalculatePerimeter() => 2 * Math.PI * this.Radius;
+
+        public override string Draw() => base.Draw() + this.GetType().Name;
+    }
+}
---
+++ Polymorphism - Lab/03. Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public double Height
+        {
+            get => this.height;
+            private set => this.height = value;
+        }
+        public double Width
+        {
+            get => this.width;
+            set => this.width = value;
+        }
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public override double CalculateArea() => this.Height * this.Width;
+
+        public override double CalculatePerimeter() => 2 * this.Height + 2 * this.Width;
+
+        public override string Draw() => base.Draw() + this.GetType().Name;
+    }
+}
