9b66216b1cd5308d874900ea9eb2902e241f787e
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-21 12:03:16 +0300

    Add files via upload

---
+++ Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Square_Root
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(Sqrt(num));
+            }
+            catch (Exception aoore)
+            {
+                Console.WriteLine(aoore.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static double Sqrt (double value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Invalid number.");
+            }
+            return Math.Sqrt(value);
+        }
+    }
+}
---
+++ Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _02._Enter_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNums = 10;
+            int count = 0;
+
+            var nums = new List<int>();
+            while (count != totalNums)
+            {
+                try
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    if (!nums.Any())
+                    {
+                        ReadNumber(1, 100, n);
+                        nums.Add(n);
+                        count++;
+                        continue;
+                    }
+
+                    ReadNumber(nums[nums.Count - 1], 100, n);
+                    nums.Add(n);
+                    count++;
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(string.Join(", ", nums));
+        }
+
+        public static void ReadNumber(int start, int end, int n)
+        {
+            if (n <= start || end <= n)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+        }
+    }
+}
