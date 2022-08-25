d09dc0a03adb385b980d802d49b40c6939827cdb
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-08-25 23:06:42 +0300

    Add files via upload

---
+++ Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,36 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string spy { get; set; }
+        public Spy()
+        {
+            
+        }
+
+        public string StealFieldInfo(string invastigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(invastigatedClass);
+            FieldInfo[] classFields = classType.GetFields(
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.NonPublic |
+                BindingFlags.Public);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Object classIntance = Activator.CreateInstance(classType, new object[] { });
+            stringBuilder.AppendLine($"Class under investigation: {invastigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classIntance)}");
+            }
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/02. High Quality Mistakes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            string result1 = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result1);
+        }
+    }
+}
