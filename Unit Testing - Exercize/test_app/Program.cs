6473fe3e5b336185bd5eb7defccc85171615ce86
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-09-04 13:08:50 +0300

    Add files via upload

---
+++ Unit Testing - Exercize/test_app/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace test_app
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HeroRepository<string> asd = new HeroRepository<string>();
+            asd.Add("car");
+            Console.WriteLine(String.Join(' ', asd));
+            Console.WriteLine(asd.FindByName("car"));
+        }
+    }
+
+    public class HeroRepository<T>
+    {
+        public List<T> hero => new List<T>();
+        public IReadOnlyCollection<T> Models => hero;
+
+        public void Add(T model)
+        {
+            hero.Add(model);
+        }
+
+        public T FindByName(string name)
+        {
+            var he = hero.FirstOrDefault(h => h.Equals(name));
+            if (he != null)
+            {
+                return he;
+            }
+            return hero.Min();
+        }
+    }
+}
